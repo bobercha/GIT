ddc7148c0d24fcfa326dde321a122140b1140001
suslik <suslyandia@gmail.com>
2020-10-15 16:58:28 +0300

    leave 200

--- src/GIT.java
+++ src/GIT.java
@@ -1,11 +1,7 @@
 public class GIT {
     public static void main(String[] args) {
-<<<<<<< HEAD
 
         for (int i = 0; i < 200; i++) {
-=======
-        for (int i = 0; i <150 ; i++) {
->>>>>>> fd386f22dcc2919223dc1fe9640a04e7c36ad091
 
         }
     }
